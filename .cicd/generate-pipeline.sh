20a40eef98be57e58f880893520a39a6c16f432d
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-07 14:22:58 -0400

    Fix issue blocking package building.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -426,7 +426,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':centos: CentOS 7.6 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "centos-7.6-pinned"
+      IMAGE_TAG: "centos-7.6-$PLATFORM_TYPE"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "el7" # OS and PKGTYPE required for lambdas
@@ -436,27 +436,12 @@ cat <<EOF
     timeout: ${TIMEOUT:-10}
     skip: ${SKIP_CENTOS_7_6}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-  - label: ":ubuntu: Ubuntu 16.04 - Package Builder"
-    command:
-      - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 16.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
-      - "./.cicd/package.sh"
-    env:
-      IMAGE_TAG: "ubuntu-16.04-pinned"
-      BUILDKITE_AGENT_ACCESS_TOKEN:
-      PLATFORM_TYPE: $PLATFORM_TYPE
-      OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
-      PKGTYPE: "deb"
-    agents:
-      queue: "$BUILDKITE_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_UBUNTU_16_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
-
   - label: ":ubuntu: Ubuntu 18.04 - Package Builder"
     command:
       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 18.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "ubuntu-18.04-pinned"
+      IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
