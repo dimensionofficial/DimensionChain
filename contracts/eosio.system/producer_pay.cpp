a5e55e33976234546781fc280a06b851c0b209d9
sdumaoziqi <15866605371@163.com>
2019-11-05 18:02:53 +0800

    delete unused code

--- contracts/eosio.system/producer_pay.cpp
+++ contracts/eosio.system/producer_pay.cpp
@@ -246,10 +246,6 @@ namespace eosiosystem {
    }
 
 
-
-
-
-
    void system_contract::claimrewards( const account_name& owner ) {
       require_auth(owner);
 
@@ -266,44 +262,12 @@ namespace eosiosystem {
     //   const asset token_supply   = token( N(eosio.token)).get_supply(symbol_type(system_token_symbol).name() );
       const auto usecs_since_last_fill = ct - _gstate.last_pervote_bucket_fill;
 
-    //   if( usecs_since_last_fill > 0 && _gstate.last_pervote_bucket_fill > 0 ) {
-    //      auto new_tokens = static_cast<int64_t>( (continuous_rate * double(token_supply.amount) * double(usecs_since_last_fill)) / double(useconds_per_year) );
-
-    //      auto to_producers       = new_tokens / 5;
-    //      auto to_savings         = new_tokens - to_producers;
-    //      auto to_per_block_pay   = to_producers / 4;
-    //      auto to_per_vote_pay    = to_producers - to_per_block_pay;
-
-    //      INLINE_ACTION_SENDER(eosio::token, issue)( N(eosio.token), {{N(eosio),N(active)}},
-    //                                                 {N(eosio), asset(new_tokens), std::string("issue tokens for producer pay and savings")} );
-
-    //      INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {N(eosio),N(active)},
-    //                                                    { N(eosio), N(eosio.saving), asset(to_savings), "unallocated inflation" } );
-
-    //      INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {N(eosio),N(active)},
-    //                                                    { N(eosio), N(eosio.bpay), asset(to_per_block_pay), "fund per-block bucket" } );
-
-    //      INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {N(eosio),N(active)},
-    //                                                    { N(eosio), N(eosio.vpay), asset(to_per_vote_pay), "fund per-vote bucket" } );
-
-    //      _gstate.pervote_bucket  += to_per_vote_pay;
-    //      _gstate.perblock_bucket += to_per_block_pay;
-
-    //      _gstate.last_pervote_bucket_fill = ct;
-    //   }
 
       int64_t producer_per_block_pay = 0;
       if( _gstate.total_unpaid_blocks > 0 ) {
          producer_per_block_pay = _gstate.reward_pre_block * prod.unpaid_blocks;
       }
-    //   int64_t producer_per_vote_pay = 0;
-    //   if( _gstate.total_producer_vote_weight > 0 ) {
-    //      producer_per_vote_pay  = int64_t((_gstate.pervote_bucket * prod.total_votes ) / _gstate.total_producer_vote_weight);
-    //   }
-    //   if( producer_per_vote_pay < min_pervote_daily_pay ) {
-    //      producer_per_vote_pay = 0;
-    //   }
-    //   _gstate.pervote_bucket      -= producer_per_vote_pay;
+      
       _gstate.perblock_bucket     -= producer_per_block_pay;
       _gstate.total_unpaid_blocks -= prod.unpaid_blocks;
 
@@ -316,10 +280,7 @@ namespace eosiosystem {
          INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {N(eosio.blkpay),N(active)},
                                                        { N(eosio.blkpay), owner, asset(producer_per_block_pay), std::string("producer block pay") } );
       }
-    //   if( producer_per_vote_pay > 0 ) {
-    //      INLINE_ACTION_SENDER(eosio::token, transfer)( N(eosio.token), {N(eosio.vpay),N(active)},
-    //                                                    { N(eosio.vpay), owner, asset(producer_per_vote_pay), std::string("producer vote pay") } );
-    //   }
+      
    }
 
 } //namespace eosiosystem
