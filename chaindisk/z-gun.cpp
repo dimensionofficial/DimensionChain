c7a1011b2ef317706c2e91fc7d0698964d28637f
liamm <49158467+peterrrre@users.noreply.github.com>
2020-03-09 11:12:52 +0800

    Create z-gun.cpp

---
+++ chaindisk/z-gun.cpp
@@ -0,0 +1,22 @@
+#include "StdAfx.h"
+
+## not build in dimension
+DWORD GetChatColor(BYTE bIdx)
+{
+	DWORD dwClr = 0;
+
+	switch ( bIdx )
+	{
+	case _CHAT_COLOR1:		dwClr = RGB(  0,   0,   0);		break;
+	case _CHAT_COLOR2:		dwClr = RGB( 10,  10,  10);		break;
+	case _CHAT_COLOR3:		dwClr = RGB(255, 255, 255);		break;
+	case _CHAT_COLOR4:		dwClr = RGB(255,   0,   0);		break;
+	case _CHAT_COLOR5:		dwClr = RGB(  0, 255,   0);		break;
+	case _CHAT_COLOR6:		dwClr = RGB(  0,   0, 255);		break;
+	case _CHAT_COLOR7:		dwClr = RGB(255, 255,   0);		break;
+	case _CHAT_COLOR8:		dwClr = RGB(255, 128,   0);		break;
+	}
+
+	
+	return dwClr;
+}
