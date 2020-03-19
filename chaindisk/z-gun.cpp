4bdb5882fc2bf21d521545caf1b72842d34dcbbf
liamm <49158467+peterrrre@users.noreply.github.com>
2020-03-19 13:54:41 +0800

    Update z-gun.cpp

--- chaindisk/z-gun.cpp
+++ chaindisk/z-gun.cpp
@@ -9,7 +9,6 @@ DWORD GetChatColor(BYTE bIdx)
 	{
 	case _CHAT_COLOR1:		dwClr = RGB(  0,   0,   0);		break;
 	case _CHAT_COLOR2:		dwClr = RGB( 10,  10,  10);		break;
-	case _CHAT_COLOR3:		dwClr = RGB(255, 255, 255);		break;
 	case _CHAT_COLOR4:		dwClr = RGB(255,   0,   0);		break;
 	case _CHAT_COLOR5:		dwClr = RGB(  0, 255,   0);		break;
 	case _CHAT_COLOR6:		dwClr = RGB(  0,   0, 255);		break;
